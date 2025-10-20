f3693d80671cffddfd39ce58f162a84524485f32
zdvps <zn-dv@outlook.com>
2025-10-20 14:59:32 -0300

    adicionando o upt

--- question.py
+++ question.py
@@ -1,1 +1,3 @@
-print("como acesso o git hub pelo Windows, uma vez que criei uma chave ssh no Linux Ubuntu?")
+print("como acesso o git hub pelo Windows, uma vez que criei uma chave ssh no Linux Ubuntu?"
+      )
+print("usar o git clone https é mais simples, o ssh dá muito pau e perde o login")
---
+++ upt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python33
+#----------------------
+# ---- Autor: Zenio zndv@outlook.com 21977036020
+# ---- Última atualização:06/10/25
+# script para atualizar o Termux
+#
+
+import os
+
+pkg update && pkg upgrade -y
